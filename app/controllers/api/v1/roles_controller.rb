fcbf89aed3e800dda110d0ee33df60559c5c69ba
Danny Lee <dreamer@panix.com>
2019-12-14 16:11:02 -0500

    add methods to Role and UserRole controllers

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::RolesController < ApplicationController
+	def index
+    @roles = Role.all
+    render json: @roles
+  end
+	
+  def show
+    @role = Role.find(params[:id])
+    render json: @role
+	end
+
+	private
+
+  def user_params
+    # whitelist params
+    params.permit(:name, :id)
+	end
 end
--- app/controllers/api/v1/user_roles_controller.rb
+++ app/controllers/api/v1/user_roles_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::UserRolesController < ApplicationController
+  def index
+    @userRoles = UserRole.all
+    render json: @userRoles
+  end
+	
+  def show
+    @userRole = UserRole.find(params[:id])
+    render json: @userRole
+	end
+
+	private
+
+  def user_params
+    # whitelist params
+    params.permit(:name, :years_exp)
+	end
 end
