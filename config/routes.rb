e64493240eed491760575a514974209ea5613fb4
Danny Lee <dreamer@panix.com>
2019-12-14 22:54:49 -0500

    Update routes, for defensive coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :users, only: [:index, :show, :create, :destroy, :update]
-      resources :teams, only: [:index, :show, :create, :destroy, :update]
-      resources :positions, only: [:index, :show, :create, :destroy, :update]
-      resources :user_roles, only: [:index, :show, :create, :destroy, :update]
-      resources :roles, only: [:index, :show, :create, :destroy, :update]
+      resources :users, only: [:index, :show, :create]
+      resources :teams, only: [:index, :show, :create]
+      resources :positions, only: [:index, :show, :create]
+      resources :user_roles, only: [:index, :show]
+      resources :roles, only: [:index, :show]
     end
   end
 end
